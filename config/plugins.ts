c42937ffae3b59b53aed5f4c10897fbc5d17a29d
LOLobito1234 <jorgitoeltroll@gmail.com>
2026-04-02 13:16:29 -0600

    Listo para producción con Cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,16 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-export default config;
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('API_KEY_CLOUDINARY'),
+        api_secret: env('API_SECRET_CLOUDINARY'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
